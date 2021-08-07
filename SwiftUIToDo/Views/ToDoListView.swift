61d1ca235335524d11be6ce9f55ebe03645fae04
burita083 <s.naka0523@gmail.com>
2021-08-07 13:41:57 +0900

    add float button and modal view

--- SwiftUIToDo/Views/ToDoListView.swift
+++ SwiftUIToDo/Views/ToDoListView.swift
@@ -10,15 +10,67 @@ import SwiftUI
 struct ToDoListView: View {
     @ObservedObject private var todoListViewModel = ToDoListViewModel()
     
+    @State private var isShown = false
     var body: some View {
-        List(todoListViewModel.items, id: \.self) { item in
-            Text(item)
+        NavigationView {
+            ZStack {
+                List(todoListViewModel.items, id: \.self) { item in
+                    Text(item)
+                }
+                .navigationBarTitle("ToDoList", displayMode: .inline)
+                
+                VStack {
+                    Spacer()
+                    
+                    HStack {
+                        Spacer()
+                        
+                        Button(action: {
+                            //self.items.append(Item(value: "Item"))
+                            isShown.toggle()
+                        }, label: {
+                            Text("+")
+                                .font(.system(.largeTitle))
+                                .frame(width: 77, height: 70)
+                                .foregroundColor(Color.white)
+                                .padding(.bottom, 7)
+                        })
+                        .background(Color.blue)
+                        .cornerRadius(38.5)
+                        .padding()
+                        .shadow(color: Color.black.opacity(0.3),
+                                radius: 3,
+                                x: 3,
+                                y: 3)
+                    }
+                }
+                
+            }
+            
+            .sheet(isPresented: $isShown) {
+                SheetView()
+            }
         }
     }
 }
 
 struct ToDoListView_Previews: PreviewProvider {
     static var previews: some View {
-        ToDoListView()
+        Group {
+            ToDoListView()
+        }
+    }
+}
+
+struct SheetView: View {
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body: some View {
+        Button("Press to dismiss") {
+            presentationMode.wrappedValue.dismiss()
+        }
+        .font(.title)
+        .padding()
+        .background(Color.black)
     }
 }
